df8b947ded3560dc2c0c9c6032b35695e1fcb2a3
Patrikão <patrikrutter7@gmail.com>
2025-05-29 14:21:31 -0300

    switching elif for case

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ def menu() :
         try :
             print('| Welcome to pyGame ! |')
             nivel = int(input("| Select the nivel (1. Easy | 2. Hard) >> "))
-            if nivel == 1 :
-                niveleasy()
-            elif nivel == 2 :
-                nivelhard()
-            else : 
-                print("Invalid option, Try Again !")
-                clear_display()
+            match nivel:
+                case 1 :
+                    niveleasy()
+                case 2 :
+                    nivelhard()
+                case _ : 
+                    print("Invalid option, Try Again !")
+                    clear_display()
 
         except ValueError :
             print("Invalid option, Try Again !")
